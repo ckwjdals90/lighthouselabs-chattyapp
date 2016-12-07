c1dd736396f2f6c9299d4085269ca05987e12892
Jungmin Cha <ckwjdals90@gmail.com>
2016-12-07 04:32:56 +0000

    scroll motion done with flex:scroll, and scrolling to the down achieved with react-dom

--- chatty_client/src/MessageList.jsx
+++ chatty_client/src/MessageList.jsx
@@ -1,22 +1,32 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
+import ReactDOM from 'react-dom';
 
 class MessageList extends Component {
   render() {
     return (
       <div id="message-list">
-        {this.props.messages.map((message) => {
+        {this.props.messages.map((message, idx) => {
           return <Message
             type={message.type}
             key={message.id}
             colourScheme={message.colourScheme}
             username={message.username}
             content={message.content}
+            ref={(ref) => this['_div' + idx] = ref}
           />
         })}
       </div>
     );
   }
+  componentDidUpdate() {
+    var len = this.props.messages.length - 1;
+    const node = ReactDOM.findDOMNode(this['_div' + len]);
+    if (node) {
+      node.scrollIntoView();
+    }
+  }
+
 }
 
 export default MessageList;
